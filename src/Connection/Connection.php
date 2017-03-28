bdd70c0211254c8743d273e87dc645aebbf9b9ed
Eugene Leonovich <eugene.leonovich@helloprint.com>
2017-03-28 15:55:40 +0200

    Add PHPDocs to the Connection interface

--- src/Connection/Connection.php
+++ src/Connection/Connection.php
@@ -4,8 +4,31 @@ namespace Tarantool\Client\Connection;
 
 interface Connection
 {
+    /**
+     * Opens a new connection.
+     *
+     * @return string A session salt
+     */
     public function open();
+
+    /**
+     * Closes an opened connection.
+     */
     public function close();
+
+    /**
+     * Indicates whether a connection is closed.
+     *
+     * @return bool
+     */
     public function isClosed();
+
+    /**
+     * Sends a msgpack request and gets a msgpack response back.
+     *
+     * @param string $data
+     *
+     * @return string
+     */
     public function send($data);
 }
