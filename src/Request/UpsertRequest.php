0550f640fee69dda5ce8bffe985bed72c72f508a
Zhenya <ja1cap@me.com>
2017-04-19 11:28:23 +0300

    Upsert request (#23)

---
+++ src/Request/UpsertRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tarantool\Client\Request;
+
+use Tarantool\Client\IProto;
+
+class UpsertRequest implements Request
+{
+
+    private $spaceId;
+    private $values;
+    private $operations;
+
+    public function __construct($spaceId, $values, $operations)
+    {
+        $this->spaceId = $spaceId;
+        $this->values = $values;
+        $this->operations = $operations;
+    }
+
+    public function getType()
+    {
+        return self::TYPE_UPSERT;
+    }
+
+    public function getBody()
+    {
+        return [
+            IProto::SPACE_ID => $this->spaceId,
+            IProto::TUPLE => $this->values,
+            IProto::OPERATIONS => $this->operations,
+        ];
+    }
+
+}
