9e91c2cd38cfabc882d2d6cc85c9c61dd3ae1924
Eugene Leonovich <gen.work@gmail.com>
2019-01-13 00:57:03 +0100

    Enhance integration tests

--- tests/Integration/ClientBuilder.php
+++ tests/Integration/ClientBuilder.php
@@ -35,7 +35,7 @@ final class ClientBuilder
     private $uri;
     private $connectionOptions = [];
 
-    public function setPacker($packer) : self
+    public function setPacker(string $packer) : self
     {
         $this->packer = $packer;
 
@@ -76,7 +76,7 @@ final class ClientBuilder
         return $this;
     }
 
-    public function setPort($port) : self
+    public function setPort(int $port) : self
     {
         $host = parse_url($this->uri, PHP_URL_HOST);
         $this->uri = sprintf('tcp://%s:%d', $host ?: self::DEFAULT_TCP_HOST, $port);
@@ -121,7 +121,7 @@ final class ClientBuilder
 
         if ($builder->isTcpConnection()) {
             $builder->setHost('0.0.0.0');
-            $builder->setPort(random_int(1024, 65535));
+            $builder->setPort(self::findOpenTcpPort(8000));
         } else {
             $builder->setUri(sprintf('unix://%s/tnt_client_%s.sock', sys_get_temp_dir(), bin2hex(random_bytes(10))));
         }
@@ -160,5 +160,24 @@ final class ClientBuilder
         }
 
         throw new \UnexpectedValueException(sprintf('"%s" packer is not supported.', $this->packer));
+    }
+
+    private static function findOpenTcpPort(int $min): int
+    {
+        $maxTries = 10;
+        $try = 0;
+
+        while (true) {
+            $port = $min + $try * 500 + random_int(1, 500);
+            if (!$fp = @stream_socket_client("tcp://127.0.0.1:$port", $errorCode, $errorMessage, 1)) {
+                return $port;
+            }
+
+            fclose($fp);
+
+            if (++$try === $maxTries) {
+                throw new \RuntimeException('Failed to find open tcp port.');
+            }
+        }
     }
 }
