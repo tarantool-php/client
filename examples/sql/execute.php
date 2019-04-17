fd5a888bf85b23a22bfcd7ea821dd9c5cb21ca0e
Eugene Leonovich <gen.work@gmail.com>
2019-04-18 00:18:18 +0200

    Simplify examples/sql/execute.php

--- examples/sql/execute.php
+++ examples/sql/execute.php
@@ -17,30 +17,25 @@ $client = create_client();
 $client->executeUpdate('DROP TABLE IF EXISTS users');
 
 $result1 = $client->executeUpdate('
-    CREATE TABLE users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username VARCHAR (100),
-        email VARCHAR(255)
-    )
+    CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, email VARCHAR(255))
 ');
 
 $result2 = $client->executeUpdate(
-    'INSERT INTO users VALUES (null, :username, :email)',
-    [':username' => 'foobar'],
+    'INSERT INTO users VALUES (null, :email)',
     [':email' => 'foobar@example.com']
 );
 
 $result3 = $client->executeQuery(
-    'SELECT username FROM users WHERE email = ?',
+    'SELECT * FROM users WHERE email = ?',
     'foobar@example.com'
 );
 
-printf("Result 1: %s\n", json_encode(['cnt' => $result1->count(), 'ids' => $result1->getAutoincrementIds()]));
+printf("Result 1: %s\n", json_encode($result1->count()));
 printf("Result 2: %s\n", json_encode(['cnt' => $result2->count(), 'ids' => $result2->getAutoincrementIds()]));
 printf("Result 3: %s\n", json_encode(iterator_to_array($result3)));
 
 /* OUTPUT
-Result 1: {"cnt":1,"ids":null}
+Result 1: 1
 Result 2: {"cnt":1,"ids":[1]}
-Result 3: [{"USERNAME":"foobar"}]
+Result 3: [{"ID":1,"EMAIL":"foobar@example.com"}]
 */
