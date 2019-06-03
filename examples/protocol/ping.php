69666c07a889e7563ee77c4e1dac06aa802e28e8
Eugene Leonovich <gen.work@gmail.com>
2019-06-03 21:26:37 +0200

    Add ping example

---
+++ examples/protocol/ping.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of the Tarantool Client package.
+ *
+ * (c) Eugene Leonovich <gen.work@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+require __DIR__.'/../bootstrap.php';
+
+$client = create_client();
+$client->ping();
