e8e4ca390954947cea7e7c0a017f82d281d47da9
Eugene Leonovich <gen.work@gmail.com>
2019-09-30 22:47:31 +0200

    Remove the ext-decimal requirement for unsupported environments

--- dockerfile.sh
+++ dockerfile.sh
@@ -12,15 +12,19 @@ if [[ -z "$TNT_CONN_URI" ]]; then
     TNT_CONN_URI='tcp://tarantool:3301'
 fi
 
+COMPOSER_REMOVE=''
+
 if [[ $TNT_PACKER == pecl ]]; then
     RUN_CMDS="$RUN_CMDS && \\\\\n    pecl install msgpack && docker-php-ext-enable msgpack"
-    COMPOSER_REMOVE='rybakit/msgpack'
+    COMPOSER_REMOVE="$COMPOSER_REMOVE rybakit/msgpack ext-decimal"
 else
     if [[ $TNT_PACKER == pure ]] && [[ $TNT_IMAGE == *"2"* ]]; then
         RUN_CMDS="$RUN_CMDS && \\\\\n    apt-get install -y libmpdec-dev"
         RUN_CMDS="$RUN_CMDS && \\\\\n    pecl install decimal && docker-php-ext-enable decimal"
+    else
+      COMPOSER_REMOVE="$COMPOSER_REMOVE ext-decimal"
     fi
-    COMPOSER_REMOVE='ext-msgpack'
+    COMPOSER_REMOVE="$COMPOSER_REMOVE ext-msgpack"
 fi
 
 if [[ -n "$COVERAGE_FILE" ]]; then
